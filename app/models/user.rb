f9f489b1c4e315d570b0596c5fc27cc0b56ac089
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-05-11 11:09:41 +0200

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+
+  has_many :microposts, dependent: :destroy
+
   attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
+
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
